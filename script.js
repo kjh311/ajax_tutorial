f02ec20c495c7ededdf43c63fc64319c7b4d4be4
Kevin Huelsmann <kjh311@gmail.com>
2017-01-20 10:26:31 -0800

    first

--- script.js
+++ script.js
@@ -16,50 +16,19 @@ $("#secondButton").on( "click", function(  ) {
   xhttp.send();
 });
 
-// $("#thirdButton").on( "click", function(  ) {
-// $.getJSON("https://kjh311.github.io/ajax_tutorial/third.json", function(result){
-//         $.each(result, function(i, field){
-//             $('#thirdDiv').html('<p> Color Name: ' + result.colorName + '</p>');
-//             $('#thirdDiv').append('<p>Hex Value : ' + result.hexValue+ '</p>');
-//         });
-//     });
-
-// $.getJSON( "https://kjh311.github.io/ajax_tutorial/third.json", function( data ) {
-//   var items = [];
-//   $.each( data, function( key, val ) {
-//     items.push( "<li id='" + key + "'>" + val + "</li>" );
-//   });
-
-//   $( "<ul/>", {
-//     "class": "my-new-list",
-//     html: items.join( "" )
-//   }).appendTo( "body" );
-// });
-// });
-
 $("#thirdButton").click(function(event){
-               $.getJSON("https://kjh311.github.io/ajax_tutorial/third.json", function(jd) {
-                  $('#thirdDiv').html('<p> Name: ' + jd.name + '</p>');
-                  $('#thirdDiv').append('<p>Age : ' + jd.age+ '</p>');
-                  $('#thirdDiv').append('<p> Sex: ' + jd.sex+ '</p>');
-                  $('#thirdDiv').append('<p> Description: ' + jd.Description+ '</p>');
+     $.getJSON("https://kjh311.github.io/ajax_tutorial/third.json", function(jd) {
+        $('#thirdDiv').html('<p> Name: ' + jd.name + '</p>');
+        $('#thirdDiv').append('<p>Age : ' + jd.age+ '</p>');
+        $('#thirdDiv').append('<p> Sex: ' + jd.sex+ '</p>');
+        $('#thirdDiv').append('<p> Description: ' + jd.Description+ '</p>');
     });
   });
 
-// $("#thirdButton").on( "click", function(  ) {
-// $.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function(data) {
-//     $('#thirdDiv').append(data);
-//     console.log(data);
-// });
-// });
-// $("#thirdButton").on( "click", function(  ) {
-// $.getJSON("https://kjh311.github.io/ajax_tutorial/third.json",
-//         function(data){
-//           $.each(data.colorsArrays, function(i,colorsArray){
-//             content = '<p>' + colorsArray.colorName + '</p>';
-//             content += '<p>' + colorsArray.hexValue + '</p>';
-//             $(content).appendTo("#thirdDiv");
-//           });
-//         });
-//     });
+$("#thirdButton").click(function(event){
+  $( "#thirdDiv2" ).load( "https://kjh311.github.io/ajax_tutorial/second.html #json-code", function( html ) {
+    });
+});
+
+
 });
