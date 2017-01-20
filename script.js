f87390f5ba6640a64a81bee7612a78c0afcadc06
Kevin Huelsmann <kjh311@gmail.com>
2017-01-20 08:54:52 -0800

    first

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     // alert( "ready!" );
 
 $("#firstButton").on( "click", function(  ) {
-  $( "#firstDiv" ).load( "/first.html" );
+  $( "#firstDiv" ).load( "https://kjh311.github.io/ajax_tutorial/first.html" );
 });
 
 
